aff0b98d0c152fd2315bac0724709d89620b6b23
Aitva <arod.louis@gmail.com>
2016-09-28 08:44:59 +0200

    First version of OrderHandler. Unfinished test for ordering.

--- order.go
+++ order.go
@@ -1,10 +1,13 @@
 package htcp
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+)
 
 const (
 	// OrderInvalid represent an invalid or uninitialized OrderType
-	OrderInvalid OrderType = iota
+	OrderInvalid Order = iota
 	// OrderCommand demands command line ordering
 	OrderCommand
 	// OrderFirstOK demands status code ordering starting with valid status
@@ -13,69 +16,87 @@ const (
 	OrderFirstKO
 )
 
-// ErrOrderTypeInvalid is return when trying to create an order type from an invalid string
-var ErrOrderTypeInvalid = errors.New("invalid OrderType")
+// ErrOrderInvalid is return when trying to create an order type from an invalid string
+var ErrOrderInvalid = errors.New("invalid Order string")
 
-// OrderType describes all possible ordering for http answer.
-type OrderType int
+// Order describes all possible ordering for http answer.
+type Order int
 
-// NewOrderType create a new OrderType from a string.
-func NewOrderType(str string) (OrderType, error) {
-	valid := map[string]OrderType{
+// ParseOrder create a new Order from a string.
+func ParseOrder(str string) (Order, error) {
+	valid := map[string]Order{
 		"command":  OrderCommand,
 		"first-ko": OrderFirstOK,
 		"first-ok": OrderFirstOK,
 	}
 	t, ok := valid[str]
 	if !ok {
-		return OrderInvalid, ErrOrderTypeInvalid
+		return OrderInvalid, ErrOrderInvalid
 	}
 	return t, nil
 }
 
-// func (c *Copy) orderKO(responses []*http.Response) []*http.Response {
-// 	valid := make([]*http.Response, 0, len(responses))
-// 	invalid := make([]*http.Response, 0, len(responses))
-//
-// outter_loop:
-// 	for _, r := range responses {
-// 		for _, code := range c.StatusCodes {
-// 			if r.StatusCode == code {
-// 				valid = append(valid, r)
-// 				continue outter_loop
-// 			}
-// 		}
-// 		invalid = append(invalid, r)
-// 	}
-// 	return append(invalid, valid...)
-// }
-//
-// func (c *Copy) orderOK(responses []*http.Response) []*http.Response {
-// 	valid := make([]*http.Response, 0, len(responses))
-// 	invalid := make([]*http.Response, 0, len(responses))
-//
-// outter_loop:
-// 	for _, r := range responses {
-// 		for _, code := range c.StatusCodes {
-// 			if r.StatusCode == code {
-// 				valid = append(valid, r)
-// 				continue outter_loop
-// 			}
-// 		}
-// 		invalid = append(invalid, r)
-// 	}
-// 	return append(valid, invalid...)
-// }
-//
-// func (c *Copy) order(responses []*http.Response) []*http.Response {
-// 	switch c.Order {
-// 	case OrderCommand:
-// 		return responses
-// 	case OrderFirstKO:
-// 		return c.orderKO(responses)
-// 	case OrderFirstOK:
-// 		return c.orderOK(responses)
-// 	default:
-// 		return responses
-// 	}
-// }
+// OrderHandler is an HTTP middleware handling response ordering.
+type OrderHandler struct {
+	next   Handler
+	Order  Order
+	CodeOK []int
+}
+
+// NewOrderHandler instantiate an OrderHandler.
+func NewOrderHandler(next Handler, o Order, codeOK []int) *OrderHandler {
+	return &OrderHandler{
+		next:   next,
+		Order:  o,
+		CodeOK: codeOK,
+	}
+}
+
+func (o *OrderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	copy, ok := FromCopyContext(r.Context())
+	if !ok {
+		return 500, errors.New("fail to retrieve CopyHandler from context")
+	}
+	switch o.Order {
+	case OrderCommand:
+		// Nothing to do.
+	case OrderFirstOK:
+		copy.Responses = o.OrderOK(copy.Responses)
+	case OrderFirstKO:
+		copy.Responses = o.OrderKO(copy.Responses)
+	default:
+		return 500, errors.New("invalid order request")
+	}
+	return o.next.ServeHTTP(w, r)
+}
+
+func (o *OrderHandler) sortByCode(responses []*http.Response) (valid, invalid []*http.Response) {
+	valid = make([]*http.Response, 0, len(responses))
+	invalid = make([]*http.Response, 0, len(responses))
+
+outter_loop:
+	for _, r := range responses {
+		for _, code := range o.CodeOK {
+			if r.StatusCode == code {
+				valid = append(valid, r)
+				continue outter_loop
+			}
+		}
+		invalid = append(invalid, r)
+	}
+	return valid, invalid
+}
+
+// OrderKO order http.Response by status code. It puts the requests
+// with a code outside OrderHandler.CodeOK first in the returned slice.
+func (o *OrderHandler) OrderKO(responses []*http.Response) []*http.Response {
+	valid, invalid := o.sortByCode(responses)
+	return append(invalid, valid...)
+}
+
+// OrderOK order http.Response by status code. It puts the requests
+// with a code inside OrderHandler.CodeOK first in the returned slice.
+func (o *OrderHandler) OrderOK(responses []*http.Response) []*http.Response {
+	valid, invalid := o.sortByCode(responses)
+	return append(valid, invalid...)
+}
